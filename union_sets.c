1723f14036043a02e3bdaa686406dba9ec5ce2da
shubhipathak11 <90844602+shubhipathak11@users.noreply.github.com>
2021-11-10 23:40:05 +0530

    Update union_sets.c

--- union_sets.c
+++ union_sets.c
@@ -3,16 +3,12 @@
 void main()
 {
    int a[10],b[10],i,c[10],j,k=0,n1,n2;
-
    
-
    printf("Enter number of  element of set A\n");
    scanf("%d",&n1);
    printf("Enter the element of set A \n");
    for(i=0;i<n1;i++)
       scanf("%d",&a[i]);
-
-    
 
    printf("Enter number of element of set B\n");
    scanf("%d",&n2);
@@ -20,21 +16,19 @@ void main()
    for(i=0;i<n2;i++)
      scanf("%d",&b[i]);
 
-   
    for(i=0;i<n1;i++)
    {
-       
-      for(j=0;j<k;j++)
+       for(j=0;j<k;j++)
       {
          if(c[j]==a[i]) 
             break;
-       }
-       if(j==k)  
-       {
-          c[k]=a[i];
-          k++;
-       }
-    }
+      }
+      if(j==k)  
+      {
+         c[k]=a[i];
+         k++;
+      }
+   }
     
    for(i=0;i<n2;i++)
    {
